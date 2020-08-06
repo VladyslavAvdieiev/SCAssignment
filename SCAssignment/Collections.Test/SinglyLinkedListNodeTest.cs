f6de890120af667bffd54e1bc9179d01c380f7df
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2020-08-06 20:22:01 +0300

    feat: create singly linked list

---
+++ SCAssignment/Collections.Test/SinglyLinkedListNodeTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Collections.Test {
+    public class SinglyLinkedListNodeTest {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("item1")]
+        public void Should_InitializeProperties(string expectedItem) {
+            SinglyLinkedListNode actualNode = new SinglyLinkedListNode(expectedItem);
+            SinglyLinkedListNode expectedNode = new SinglyLinkedListNode(expectedItem);
+            actualNode.Next = expectedNode;
+
+            Assert.Equal(expectedItem, actualNode.Value);
+            Assert.Equal(expectedNode, actualNode.Next);
+        }
+    }
+}
